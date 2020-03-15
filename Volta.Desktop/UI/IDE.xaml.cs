69d4f5aedc9d0bf1573b9b4ee3b921c683791efd
Miguel Rivas <migueladanrm@outlook.com>
2020-03-14 19:04:30 -0600

    Preparación inicial del proyecto

---
+++ Volta.Desktop/UI/IDE.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Volta.UI
+{
+    /// <summary>
+    /// Interaction logic for IDE.xaml
+    /// </summary>
+    public partial class IDE : Window
+    {
+        public IDE() {
+            InitializeComponent();
+        }
+    }
+}
