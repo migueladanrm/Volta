f52edfc9b53a6710be9a43c34744e48842cabc91
Miguel Rivas <migueladanrm@outlook.com>
2020-06-07 20:08:55 -0600

    CLI y pruebas - Se crea proyecto de CLI. - Se crea proyecto de pruebas unitarias.

---
+++ Volta.CLI/Commands/VoltaCommand.cs
@@ -0,0 +1,32 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Volta.Compiler;
+using System.IO;
+
+namespace Volta.CLI.Commands
+{
+    [Command(Name = "volta")]
+    class VoltaCommand : ICommand
+    {
+        [Required]
+        [Option(CommandOptionType.SingleValue, LongName = "file", ShortName = "f", Description = "The repository unique name.")]
+        public string OptionFile { get; set; }
+
+        public int OnExecute() {
+            try {
+                var text = File.ReadAllText(OptionFile);
+                var errors = Controller.Check(text);
+                foreach (var vpe in errors) {
+                    Console.Error.WriteLine($"Error at Ln {vpe.Line} Col {vpe.CharPositionInLine}:\n\t{vpe.Message}");
+                }
+                return 0;
+            } catch (Exception e) {
+                Console.WriteLine(e.Message);
+                return -1;
+            }
+        }
+    }
+}
---
+++ Volta.Tests/VoltaCompilerTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Volta.Compiler;
+using System.IO;
+using System.Linq;
+
+namespace Volta.Tests
+{
+    [TestClass]
+    public class VoltaCompilerTests
+    {
+        private Dictionary<string, string> codeSamples;
+
+        [TestInitialize]
+        public void Initialize() {
+            codeSamples = new Dictionary<string, string>();
+
+            Directory.GetFiles("SampleFiles").ToList().ForEach(file => {
+                var fileName = new FileInfo(file).Name;
+                codeSamples.Add(fileName, File.ReadAllText(file));
+
+                Console.WriteLine($"Loaded '{fileName}'.");
+            });
+
+        }
+
+        [TestMethod]
+        public void CheckCodeErrors() {
+            Console.WriteLine("Checking samples...");
+
+            foreach (var kv in codeSamples) {
+                Console.WriteLine($"File: {kv.Key}");
+                var errors = Controller.Check(kv.Value);
+                foreach (var vpe in errors) {
+                    Console.Error.WriteLine($"Error at Ln {vpe.Line} Col {vpe.CharPositionInLine}:\n\t{vpe.Message}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
