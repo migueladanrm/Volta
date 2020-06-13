e4e3bb100cb7eff3ce950766f187c5138249d939
Miguel Rivas <migueladanrm@outlook.com>
2020-06-12 20:40:19 -0600

    IDE: Nueva vista de inicio - Utilidades de serialización. - Cambios en guardado de ajustes (serialización binaria).

---
+++ Volta.Desktop/Common/SerializationUtils.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Volta.Common
+{
+    public static class SerializationUtils
+    {
+        public static Stream Serialize(object obj) {
+            var stream = new MemoryStream();
+            var binaryFormatter = new BinaryFormatter();
+            binaryFormatter.Serialize(stream, obj);
+
+            return stream;
+        }
+
+        public static T Deserialize<T>(byte[] bytes) {
+            using var ms = new MemoryStream(bytes);
+            return Deserialize<T>(ms);
+        }
+
+        public static T Deserialize<T>(Stream stream) {
+            var binaryFormatter = new BinaryFormatter();
+            return (T)binaryFormatter.Deserialize(stream);
+        }
+    }
+}
