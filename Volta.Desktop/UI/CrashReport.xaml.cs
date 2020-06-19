e5ae11fde0a48c1f294d8f63c49844b930f8e686
Miguel Rivas <migueladanrm@outlook.com>
2020-06-19 17:13:36 -0600

    IDE - Ventana de reporte de error crítico. - Estructura para etapa de generación de código.

---
+++ Volta.Desktop/UI/CrashReport.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using Volta.Common;
+
+namespace Volta.UI
+{
+    /// <summary>
+    /// Interaction logic for CrashReport.xaml
+    /// </summary>
+    public partial class CrashReport : Window
+    {
+        /// <summary>
+        /// Inicializa una instancia de <see cref="CrashReport"/>.
+        /// </summary>
+        public CrashReport() {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Inicializa una instancia de <see cref="CrashReport"/>.
+        /// </summary>
+        /// <param name="crashReportFile">Archivo de datos del error crítico.</param>
+        public CrashReport(string crashReportFile) {
+            InitializeComponent();
+
+            LoadCrashReport(crashReportFile);
+        }
+
+        /// <summary>
+        /// Carga la información del error crítico.
+        /// </summary>
+        /// <param name="crashReportFile">Archivo de datos del error crítico.</param>
+        private void LoadCrashReport(string crashReportFile) {
+            if (crashReportFile == null || !File.Exists(crashReportFile))
+                return;
+
+            try {
+                var file = new FileInfo(crashReportFile);
+                var ex = SerializationUtils.Deserialize<Exception>(File.ReadAllBytes(file.FullName));
+                TbxCrashDetails.Text = $"Volta Crash Report\n{file.CreationTime:O}\n\nMessage:\n{ex.Message}";
+            } catch { }
+        }
+
+        private void BtnDone_Click(object sender, RoutedEventArgs e) {
+            Close();
+        }
+    }
+}
