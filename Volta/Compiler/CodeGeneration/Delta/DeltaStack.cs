1f7ccaf8dd805313a2b015b1155e26d007189a34
Miguel Rivas <migueladanrm@outlook.com>
2020-06-25 19:32:47 -0600

    Intérprete Delta - Estructura general del intérprete.

---
+++ Volta/Compiler/CodeGeneration/Delta/DeltaStack.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+
+namespace Volta.Compiler.CodeGeneration.Delta
+{
+    public class DeltaStack
+    {
+        private Stack stack;
+
+        public DeltaStack() {
+            stack = new Stack();
+        }
+
+        public void Push(dynamic value)
+            => stack.Push(value);
+
+        public dynamic Pop()
+            => stack.Pop();
+
+    }
+}
---
+++ Volta/Compiler/CodeGeneration/Delta/DeltaStore.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace Volta.Compiler.CodeGeneration.Delta
+{
+    public class DeltaStore
+    {
+        private Dictionary<string, dynamic> data;
+
+        public DeltaStore(string name) {
+            data = new Dictionary<string, dynamic>();
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+
+        #region Management
+
+        public void AddValue(string key, dynamic value) {
+            data.Add(key, value);
+        }
+
+        public bool ExistsValue(string key)
+            => data.ContainsKey(key);
+
+        public dynamic GetValue(string key) {
+            return data.ContainsKey(key) ? data[key] : null;
+        }
+
+        public void UpdateValue(string key, dynamic newValue) {
+            if (ExistsValue(key))
+                data[key] = newValue;
+        }
+
+        #endregion
+    }
+}
